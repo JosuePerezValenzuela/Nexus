0ad692ed9dc802e3cdabfcde263aef4bcfdd0a27
Josue Perez <josueperezv2004@gmail.com>
2025-12-16 19:05:42 -0400

    FEAT: Connection to the BD with Session and Yield

---
+++ src/app/core/session.py
@@ -0,0 +1,17 @@
+from collections.abc import Generator
+
+from sqlmodel import Session, create_engine
+
+from app.core.config import settings
+
+# Creacion del motor
+# connection_args es necesario para que psycopg funcione bien con JSONB
+engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI), echo=True)
+
+
+# Dependencia para FastAPI (The "Get DB" function)
+# Esta funcion se usara en cada endpoint que necesite base de datos
+# abre una session, la presta y la cierra automaticamente al terminar la peticion
+def get_db() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
