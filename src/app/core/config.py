b77cd2010df76d68bfbde300f1583af7013cda98
Josue Perez <josueperezv2004@gmail.com>
2025-12-16 22:52:50 +0000

    FEAT: Adding minor configuration for the DB

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -12,6 +12,7 @@ class Settings(BaseSettings):
 
     # Configuracion de Base de datos
     POSTGRES_SERVER: str
+    POSTGRES_PORT: int = 5432
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
@@ -25,6 +26,7 @@ class Settings(BaseSettings):
             username=self.POSTGRES_USER,
             password=self.POSTGRES_PASSWORD,
             host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
             path=self.POSTGRES_DB,
         )
 
