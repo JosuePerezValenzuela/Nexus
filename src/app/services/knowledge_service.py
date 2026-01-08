9a69b11009ed3be029ae1ff7b27a27ed50703a8e
Josue <josueperezv2004@gmail.com>
2026-01-08 15:17:20 +0000

    FEAT: Adding the service to process PDFs with chunks

--- src/app/services/knowledge_service.py
+++ src/app/services/knowledge_service.py
@@ -1,3 +1,10 @@
+import os
+import shutil
+from typing import Any, cast
+
+from fastapi import UploadFile
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from sqlmodel import Session, select
 
 from app.models.knowledge import KnowledgeBase
@@ -34,6 +41,61 @@ class KnowledgeService:
         statement = select(KnowledgeBase)
         results = session.exec(statement)
         return list(results.all())
+
+    async def proccess_pdf(self, session: Session, file: UploadFile):
+        """
+        Recibe un PDF, lo guarda temporalmente, lo trocea y vectoriza cada parte
+        """
+        temp_dir = "temp_uploads"
+        os.makedirs(temp_dir, exist_ok=True)
+
+        safe_filename = file.filename or "temp_document.pdf"
+
+        temp_file_path = os.path.join(temp_dir, safe_filename)
+
+        with open(temp_file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        try:
+            # Carga del pdf
+            loader = PyPDFLoader(temp_file_path)
+            docs = loader.load()  # Extrae el texto pagina por pagina
+
+            # Chunking
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=1000, chunk_overlap=200
+            )
+            splits = text_splitter.split_documents(docs)
+
+            print(f"📄 Procesando PDF: {len(splits)} fragmentos generados.")
+
+            # Procesamiento de cada fragmento
+            for split in splits:
+                vector = await llm_service.get_embedding(split.page_content)
+
+                meta = cast(dict[str, Any], split.metadata)  # type: ignore
+
+                page_num = meta.get("page", 0)
+                # Creacion del objeto a guardar en la BD
+                new_chunk = KnowledgeBase(
+                    title=f"{file.filename} - Pag {page_num}",
+                    content=split.page_content,
+                    source=safe_filename,
+                    embedding=vector,
+                )
+
+                session.add(new_chunk)
+
+            session.commit()
+            return {
+                "message": "PDF procesado exitosamente. \n"
+                f"Se crearon {len(splits)} fragmentos de conocimiento."
+            }
+
+        finally:
+            # Limpieza del archivo temporal
+            if os.path.exists(temp_file_path):
+                os.remove(temp_file_path)
 
 
 knowledge_service = KnowledgeService()
