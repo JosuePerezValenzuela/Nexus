ce31eecc5b0463311cef00c0e5450a4190434584
Josue <josueperezv2004@gmail.com>
2026-01-09 00:10:22 +0000

    FEAT: Adding System prompt for the Agentic worker RAG

--- src/app/agents/rag_agent.py
+++ src/app/agents/rag_agent.py
@@ -1,5 +1,6 @@
 from typing import Annotated, Any, Literal, TypedDict
 
+from langchain_core.messages import SystemMessage
 from langgraph.graph import START, StateGraph  # type: ignore
 from langgraph.graph.message import add_messages  # type: ignore
 from langgraph.prebuilt import ToolNode
@@ -13,6 +14,25 @@ class AgentState(TypedDict):
     messages: Annotated[list[Any], add_messages]
 
 
+SYSTEM_PROMPT = """Eres Nexus AI, un asistente experto y servicial.
+
+TIENES DOS MODOS DE OPERACIÓN:
+
+MODO 1: CHARLA CASUAL (Sin Herramientas)
+- Si es una charla general.
+-> EN ESTOS CASOS: Responde directamente y NO uses ninguna herramienta.
+
+MODO 2: BÚSQUEDA DE INFORMACIÓN (Con Herramientas)
+- Si el usuario pregunta sobre manuales, PDFs subidos o datos específicos de Bolivia.
+- Si la respuesta requiere datos exactos que no conoces de memoria.
+-> EN ESTOS CASOS: Usa 'search_knowledge_base', y cita las fuentes de donde sacaste la
+informacion.
+
+IMPORTANTE:
+- Si usas la herramienta, cita la fuente.
+- Si no encuentras nada, dilo honestamente, no inventes.
+"""
+
 # 2. Herramientas
 tools = [search_knowledge_base]
 tool_node = ToolNode(tools)
@@ -23,7 +43,12 @@ model_with_tools = llm_service.llm.bind_tools(tools)  # type: ignore
 
 # 4. Nodos
 def agent_node(state: AgentState):
-    return {"messages": [model_with_tools.invoke(state["messages"])]}
+    current_messages = state["messages"]
+
+    messages_with_rules = [SystemMessage(content=SYSTEM_PROMPT)] + current_messages
+
+    response = model_with_tools.invoke(messages_with_rules)
+    return {"messages": [response]}
 
 
 def should_continue(state: AgentState) -> Literal["tools", "__end__"]:
