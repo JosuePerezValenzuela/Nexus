6249e2be088b921875c544064bd681bd4de666f5
Josue <josueperezv2004@gmail.com>
2026-01-30 02:18:07 +0000

    FEAT: Adding the new field in the agentState

--- src/app/graph/state.py
+++ src/app/graph/state.py
@@ -6,3 +6,6 @@ from langgraph.graph.message import add_messages  # type: ignore
 class AgentState(TypedDict):
     # add_messages es el Reducer: concatena en lugar de sobreescribir
     messages: Annotated[list[Any], add_messages]
+
+    # Nuevo campo next para el supervisor y su siguiente paso
+    next: str | None
