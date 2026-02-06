19b434dd87d6b4714c1de462a9f52e4bec4aa505
Josue <josueperezv2004@gmail.com>
2026-02-06 01:08:47 +0000

    UPDATE: The lenght of the embedding to the vector database

--- src/app/models/knowledge.py
+++ src/app/models/knowledge.py
@@ -24,4 +24,4 @@ class KnowledgeBase(SQLModel, table=True):
 
     created_at: datetime = Field(default_factory=get_utc_now)
 
-    embedding: list[float] | None = Field(default=None, sa_column=Column(Vector(384)))
+    embedding: list[float] | None = Field(default=None, sa_column=Column(Vector(1024)))
